96b70b529f413880e99a82b56e2bb043b6049b4e
LucasGbro <luqueirobrog@gmail.com>
2025-06-27 23:33:19 -0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import TradeSimulatorApp from "./TradeSimulatorApp";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <TradeSimulatorApp />
+  </React.StrictMode>
+);
